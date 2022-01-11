2b2e9f5c7fb4cc6620fd368a8e354daa8e89a97a
Labrot <sarah.lientz@gmail.com config --global user.email>
2022-01-11 17:27:33 -0500

    added function to app js file that prompts collection of info on intern and employee

--- app.js
+++ app.js
@@ -36,108 +36,108 @@ function employeeInformation() {
 
 // Prompt to collect information on manager and then go back to enter a new employee
 function managerInformation() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "what is your manager's name?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "What is your manager's id",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your manager's email?",
-      name: "email"
-    },
-    {
-      type: "input",
-      message: "What is your manager's office number",
-      name: "number",
-    },
-  ]).then(function(answer) {
-    let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
-    team.push(manager);
-
-    employeeInformation()
-  })
-}; // end of function 
-
-
-// Prompt to collect information on engineer and then go back to enter a new employee
-function engineerInformation() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "what is your engineer's name?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "What is your engineer's ID",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your engineer's email?",
-      name: "email"
-    },
-    {
-      type: "input",
-      message: "What is your engineer's GitHub username",
-      name: "GitHub",
-    },
-  ]).then(function(answer) {
-    let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
-    team.push(engineer);
-
-    employeeInformation()
-  })
-}; // end of function 
-
-
-// Prompt to collect information on intern and then go back to enter a new employee
-function internInformation() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "what is your intern's name?",
-      name: "name"
-    },
-    {
-      type: "input",
-      message: "What is your intern's ID",
-      name: "id",
-    },
-    {
-      type: "input",
-      message: "What is your intern's email?",
-      name: "email"
-    },
-    {
-      type: "input",
-      message: "What is your intern's school",
-      name: "school",
-    },
-  ]).then(function(answer) {
-    let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
-    team.push(intern);
-
-    employeeInformation()
-  })
-}; // end of function 
-
-// write the answers to a new README file
-function generateHTML(fileName, data) {
-  fs.writeFile(fileName, data, "utf8", function (err) {
-    if (err) {
-      throw err;
-    }
-    console.log("You have successfully written your Employee Summary");
-  });
-};
-
-// Calls function to begin prompt for roles to enter
-employeeInformation();
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "what is your manager's name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your manager's id",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your manager's office number",
+        name: "number",
+      },
+    ]).then(function(answer) {
+      let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
+      team.push(manager);
+  
+      employeeInformation()
+    })
+  }; // end of function 
+  
+  
+  // Prompt to collect information on engineer and then go back to enter a new employee
+  function engineerInformation() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "what is your engineer's name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your engineer's ID",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your engineer's GitHub username",
+        name: "GitHub",
+      },
+    ]).then(function(answer) {
+      let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
+      team.push(engineer);
+  
+      employeeInformation()
+    })
+  }; // end of function 
+  
+  
+// ---------  Added function prompting to collect information on intern and then go back to enter a new employee
+  function internInformation() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "what is your intern's name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your intern's ID",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your intern's school",
+        name: "school",
+      },
+    ]).then(function(answer) {
+      let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+      team.push(intern);
+  
+      employeeInformation()
+    })
+  }; // end of function 
+  
+  // write the answers to a new README file
+  function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", function (err) {
+      if (err) {
+        throw err;
+      }
+      console.log("You have successfully written your Employee Summary");
+    });
+  };
+  
+  // Calls function to begin prompt for roles to enter
+  employeeInformation();
